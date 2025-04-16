76407f21ed2312681e792c13e73878deca7ddd50
Sergey Matsiupa <msvcontact@gmail.com>
2025-04-16 22:33:12 +0200

    t14

--- app/code/Overdose/LessonOne/Model/ResourceModel/LessonOne/Collection.php
+++ app/code/Overdose/LessonOne/Model/ResourceModel/LessonOne/Collection.php
@@ -2,7 +2,6 @@
 namespace Overdose\LessonOne\Model\ResourceModel\LessonOne;
 
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
-use Psr\Log\LoggerInterface;
 
 /**
  * Class Collection
@@ -11,35 +10,6 @@ use Psr\Log\LoggerInterface;
  */
 class Collection extends AbstractCollection
 {
-    /**
-     * @var LoggerInterface
-     */
-    protected $logger;
-
-    /**
-     * Constructor
-     *
-     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
-     * @param LoggerInterface $logger
-     * @param \Magento\Framework\Logger\Monolog $logger
-     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
-     * @param \Magento\Framework\Event\ManagerInterface $eventManager
-     * @param \Magento\Framework\DB\Adapter\AdapterInterface|null $connection
-     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb|null $resource
-     */
-    public function __construct(
-        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
-        LoggerInterface $logger,
-        \Magento\Framework\Logger\Monolog $loggerMonolog,
-        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
-        \Magento\Framework\Event\ManagerInterface $eventManager,
-        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
-        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
-    ) {
-        $this->logger = $logger;
-        parent::__construct($entityFactory, $fetchStrategy, $eventManager, $connection, $resource);
-    }
-
     /**
      * Define resource model
      *
@@ -58,7 +28,6 @@ class Collection extends AbstractCollection
      */
     protected function _afterLoad()
     {
-        $this->logger->debug('Collection _afterLoad called with size: ' . $this->getSize());
         return parent::_afterLoad();
     }
 }
