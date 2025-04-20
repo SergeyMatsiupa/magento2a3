6874cc6e77ce0493db54201b2bd234880c5b2c34
Sergey Matsiupa <msvcontact@gmail.com>
2025-04-20 20:55:28 +0200

    t59

--- app/code/Overdose/LessonOne/Controller/Adminhtml/Lesson/Edit.php
+++ app/code/Overdose/LessonOne/Controller/Adminhtml/Lesson/Edit.php
@@ -4,6 +4,8 @@ namespace Overdose\LessonOne\Controller\Adminhtml\Lesson;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Registry;
+use Magento\Framework\Exception\LocalizedException;
 
 class Edit extends Action
 {
@@ -13,43 +15,85 @@ class Edit extends Action
     protected $resultPageFactory;
 
     /**
+     * @var Registry
+     */
+    protected $_coreRegistry;
+
+    /**
      * Constructor
      *
      * @param Context $context
      * @param PageFactory $resultPageFactory
+     * @param Registry $registry
      */
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory
+        PageFactory $resultPageFactory,
+        Registry $registry
     ) {
-        // Call parent constructor to initialize the context
-        parent::__construct($context);
-        // Assign PageFactory to class property
         $this->resultPageFactory = $resultPageFactory;
+        $this->_coreRegistry = $registry;
+        parent::__construct($context);
     }
 
     /**
      * Execute action to display the edit lesson page
      *
-     * @return \Magento\Framework\View\Result\Page
+     * @return \Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
-        // Temporary logging to debug the controller
+        // Log execution for debugging
         \Magento\Framework\App\ObjectManager::getInstance()
             ->get(\Psr\Log\LoggerInterface::class)
             ->debug('Edit controller execute called.');
-        
-        // Create a new page object using PageFactory
-        $resultPage = $this->resultPageFactory->create();
-        // Set the page title in the admin panel
-        $resultPage->getConfig()->getTitle()->prepend(__('Edit Lesson'));
-        // Return the page to be rendered
-        return $resultPage;
 
-        // Temporary logging to confirm page creation
-        \Magento\Framework\App\ObjectManager::getInstance()
-            ->get(\Psr\Log\LoggerInterface::class)
-            ->debug('Edit controller result page created.');
+        // Get lesson_id from request
+        $id = $this->getRequest()->getParam('lesson_id');
+        $model = $this->_objectManager->create(\Overdose\LessonOne\Model\Lesson::class);
+
+        // Load existing lesson if ID is provided
+        if ($id) {
+            try {
+                $model->load($id);
+                if (!$model->getId()) {
+                    $this->messageManager->addErrorMessage(__('This lesson no longer exists.'));
+                    $resultRedirect = $this->resultRedirectFactory->create();
+                    return $resultRedirect->setPath('*/*/');
+                }
+            } catch (LocalizedException $e) {
+                $this->messageManager->addErrorMessage($e->getMessage());
+                $resultRedirect = $this->resultRedirectFactory->create();
+                return $resultRedirect->setPath('*/*/');
+            }
+        }
+
+        // Register model in registry
+        $this->_coreRegistry->register('lessonone_lesson', $model);
+
+        // Create result page
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Overdose_LessonOne::lessonone_lesson')
+            ->addBreadcrumb(__('Lessons'), __('Lessons'))
+            ->addBreadcrumb(
+                $id ? __('Edit Lesson') : __('New Lesson'),
+                $id ? __('Edit Lesson') : __('New Lesson')
+            );
+
+        // Set page title
+        $resultPage->getConfig()->getTitle()->prepend(__('Lessons'));
+        $resultPage->getConfig()->getTitle()->prepend($model->getId() ? $model->getTitle() : __('New Lesson'));
+
+        return $resultPage;
+    }
+
+    /**
+     * Authorization check
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Overdose_LessonOne::lesson');
     }
 }
